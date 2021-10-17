7de801f87beb148aa707de642d6f4d16bf117b55
johnnymeneses <bilbo_vader@outlook.com>
2021-10-17 15:51:50 -0300

    Projeto JMail

---
+++ JMail/app/src/main/java/com/johnny/jmail/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.johnny.jmail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //versão Kotlin
+        supportActionBar!!.hide()
+    }
+}
