b9ed07d5172da0cb197a95b1413eea79eb7114a7
johnnymeneses <bilbo_vader@outlook.com>
2021-10-26 11:45:20 -0300

    Ajustes Cores

---
+++ JMail/app/src/main/java/com/johnny/jmail/formLogin.kt
@@ -0,0 +1,15 @@
+package com.johnny.jmail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class formLogin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_login)
+
+//        Escondendo o header roxo
+            supportActionBar!!.hide()
+
+    }
+}
