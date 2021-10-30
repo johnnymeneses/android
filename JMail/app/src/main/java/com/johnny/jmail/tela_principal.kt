0e15574fcac87d44e537a50d3daaddb03cfff326
johnnymeneses <bilbo_vader@outlook.com>
2021-10-30 12:29:52 -0300

    Tela Principal

--- JMail/app/src/main/java/com/johnny/jmail/tela_principal.kt
+++ JMail/app/src/main/java/com/johnny/jmail/tela_principal.kt
@@ -7,5 +7,9 @@ class tela_principal : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tela_principal)
+
+        //        Escondendo o header roxo
+        supportActionBar!!.hide()
+
     }
 }
