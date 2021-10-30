5ed7233f11a55d6f2d3383d15bff700de792e314
johnnymeneses <bilbo_vader@outlook.com>
2021-10-30 12:11:29 -0300

    Tela Principal

---
+++ JMail/app/src/main/java/com/johnny/jmail/tela_principal.kt
@@ -0,0 +1,11 @@
+package com.johnny.jmail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class tela_principal : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_principal)
+    }
+}
