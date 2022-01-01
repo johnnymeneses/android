303af87a893f5316c9f9604ac961f5b47358be6b
johnnymeneses <bilbo_vader@outlook.com>
2022-01-01 15:11:35 -0300

    Gasto da Viagem

---
+++ GastodeViagem/app/src/main/java/com/johnny/gastodeviagem/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.johnny.gastodeviagem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btn_calcular = findViewById<Button>(R.id.btn_calcular)
+
+        btn_calcular.setOnClickListener(){
+            calculaViagem()
+
+        }
+
+
+
+    }
+
+
+
+    fun calculaViagem(){
+        var distancia = findViewById<EditText>(R.id.edit_distancia)
+        var preco = findViewById<EditText>(R.id.edit_preco)
+        var autonomia = findViewById<EditText>(R.id.edit_autonomia)
+        var resultado = findViewById<TextView>(R.id.txt_resultado)
+
+        val valorPreco = preco.text.toString().toDouble()
+        val valorAutonomia = autonomia.text.toString().toDouble()
+        val valorDistancia = distancia.text.toString().toDouble()
+
+        val litrosNecessarioa = valorDistancia / valorAutonomia
+        val resultadoGasto = litrosNecessarioa * valorPreco
+
+
+
+
+        resultado.text = ("R$ ${resultadoGasto}")
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+}
