05807437e9e045239d886473d05c1e077d49aaf7
johnnymeneses <bilbo_vader@outlook.com>
2022-01-11 16:49:05 -0300

    Exercicio 2 - Layout

--- Udemy2/app/src/main/java/com/johnny/udemy2/MainActivity.kt
+++ Udemy2/app/src/main/java/com/johnny/udemy2/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.johnny.udemy2
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CheckBox
+import android.widget.ImageView
+import com.johnny.udemy2.R.color.purple_200
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+
+
+
+
+}
+
+
+
+
+
+
 }
