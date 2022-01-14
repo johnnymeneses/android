47f3c627ee8bf00bf6b2302a7af0bcd43df9421d
johnnymeneses <bilbo_vader@outlook.com>
2022-01-14 11:36:26 -0300

    Refatorando projeto Layout

--- Udemy2/app/src/main/java/com/johnny/udemy2/MainActivity.kt
+++ Udemy2/app/src/main/java/com/johnny/udemy2/MainActivity.kt
@@ -10,19 +10,13 @@ import com.johnny.udemy2.R.color.purple_200
 class MainActivity : AppCompatActivity() {
 
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
-
-
-}
-
-
-
-
+        supportActionBar!!.hide()
+    }
 
 
 }
