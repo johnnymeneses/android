f73b9d8e800074472d676c92571d7310f8feea3d
johnnymeneses <bilbo_vader@outlook.com>
2022-01-30 18:01:00 -0300

    Shared Preferences

--- Motivation/app/src/main/java/com/johnny/motivation/SplashActivity.kt
+++ Motivation/app/src/main/java/com/johnny/motivation/SplashActivity.kt
@@ -1,13 +1,41 @@
 package com.johnny.motivation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_splash.*
 
-class SplashActivity : AppCompatActivity() {
+
+class SplashActivity : AppCompatActivity(), View.OnClickListener {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
         supportActionBar!!.hide()
+        btn_save.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        val id = view.id
+        if (id == R.id.btn_save) {
+            handleSave()
+        }
+    }
+
+
+    private fun handleSave() {
+        val name = editname.text.toString()
+
+
+        if (name != "") {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        } else {
+            Toast.makeText(this, "Preencha seu nome", Toast.LENGTH_SHORT).show()
+        }
     }
 }
