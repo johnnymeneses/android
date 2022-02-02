d9e35909a2b905b6ef4406367a1970b1bb8fb4fe
johnnymeneses <bilbo_vader@outlook.com>
2022-02-01 22:29:52 -0300

    refatorar

--- JBank/app/src/main/java/com/johnny/jbank/tela_login.kt
+++ JBank/app/src/main/java/com/johnny/jbank/tela_login.kt
@@ -4,20 +4,10 @@ import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ProgressBar
-import android.widget.TextView
-import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_tela_login.*
 
 class tela_login : AppCompatActivity() {
 
-    private var TextView text_telaCadastro;
-    private var EditText edit_email;
-    private var EditText edit_senha;
-    private var Button btn_login;
-    private var ProgressBar progressBar;
 
 
 
@@ -28,6 +18,9 @@ class tela_login : AppCompatActivity() {
 
         //Escondendo o header
         supportActionBar!!.hide()
+
+        btn_entrar.
+
 
     }
 
