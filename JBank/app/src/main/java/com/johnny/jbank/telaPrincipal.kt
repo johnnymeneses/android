0985d5d9b7e17b0348102a4df5a160c5dcb2d18a
johnnymeneses <bilbo_vader@outlook.com>
2022-02-06 09:39:37 -0300

    JPIX

--- JBank/app/src/main/java/com/johnny/jbank/telaPrincipal.kt
+++ JBank/app/src/main/java/com/johnny/jbank/telaPrincipal.kt
@@ -4,12 +4,8 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.ContactsContract
 import android.widget.Button
-import android.widget.EditText
 import android.widget.TextView
-import com.johnny.jbank.modelo.Cliente
-import com.johnny.jbank.modelo.contaCorrente
 
 class telaPrincipal : AppCompatActivity() {
 
