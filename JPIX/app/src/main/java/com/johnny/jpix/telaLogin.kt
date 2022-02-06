fcbdbbc3c92d079a3e2084e0eb2a36f28954e2e6
johnnymeneses <bilbo_vader@outlook.com>
2022-02-06 09:41:40 -0300

    Ajustes JPIX

---
+++ JPIX/app/src/main/java/com/johnny/jpix/telaLogin.kt
@@ -0,0 +1,11 @@
+package com.johnny.jpix
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class telaLogin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_login)
+    }
+}
