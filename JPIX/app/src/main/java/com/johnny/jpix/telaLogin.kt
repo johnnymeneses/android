6207be996fdac2e17c66465341e24a31c4e2944e
johnnymeneses <bilbo_vader@outlook.com>
2022-02-06 10:54:29 -0300

    Tela Login

--- JPIX/app/src/main/java/com/johnny/jpix/telaLogin.kt
+++ JPIX/app/src/main/java/com/johnny/jpix/telaLogin.kt
@@ -7,5 +7,10 @@ class telaLogin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tela_login)
+
+
+        supportActionBar!!.hide()
+
+
     }
 }
